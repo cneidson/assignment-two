2c2c4ec11c0740fd4d65250b073334b71e5680fc
cneidson <carrie.eidson@nyu.edu>
2020-02-13 14:32:08 -0500

    Javascript Fixed

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,19 +1,257 @@
-//Code from codepen dot io backlash jjimih
+/* Lightbox code from codepen dot io backlash jjimih */
 
 jQuery(document).ready(function(){
 
-	/* Open lightbox on button click */
-	$('.lightbox-toggle').click(function(){
-		$('.backdrop').animate({'opacity':'.50'}, 300, 'linear').css('display', 'block');
-		$('.box').fadeIn();
+	$('#lightbox-toggle').click(function(){
+		$('#backdrop').animate({'opacity':'.80'}, 300, 'linear').css('display', 'block');
+		$('#box').fadeIn();
 	});
 
-	/* Click to close lightbox */
-	$('.close, .backdrop').click(function(){
-		$('.backdrop').animate({'opacity':'0'}, 300, 'linear', function(){
-			$('.backdrop').css('display', 'none');
+	$('.close, #backdrop').click(function(){
+		$('#backdrop').animate({'opacity':'0'}, 300, 'linear', function(){
+			$('#backdrop').css('display', 'none');
 		});
-		$('.box').fadeOut();
+		$('#box').fadeOut();
 	});
+
+	/*Hover function tutorial from W3schools */
+
+	$('.close').hover(function() {
+	        $(this).css('cursor','pointer');
+	    });
+
+});
+
+/*Second pic*/
+jQuery(document).ready(function(){
+
+	$('#lightbox-toggle2').click(function(){
+		$('#backdrop2').animate({'opacity':'.80'}, 300, 'linear').css('display', 'block');
+		$('#box2').fadeIn();
+	});
+
+	$('.close, #backdrop2').click(function(){
+		$('#backdrop2').animate({'opacity':'0'}, 300, 'linear', function(){
+			$('#backdrop2').css('display', 'none');
+		});
+		$('#box2').fadeOut();
+	});
+
+	$('.close').hover(function() {
+					$(this).css('cursor','pointer');
+			});
+
+});
+
+/*Third pic*/
+jQuery(document).ready(function(){
+
+	$('#lightbox-toggle3').click(function(){
+		$('#backdrop3').animate({'opacity':'.80'}, 300, 'linear').css('display', 'block');
+		$('#box3').fadeIn();
+	});
+
+	$('.close, #backdrop3').click(function(){
+		$('#backdrop3').animate({'opacity':'0'}, 300, 'linear', function(){
+			$('#backdrop3').css('display', 'none');
+		});
+		$('#box3').fadeOut();
+	});
+
+	$('.close').hover(function() {
+					$(this).css('cursor','pointer');
+			});
+
+});
+
+/*Fourth pic*/
+jQuery(document).ready(function(){
+
+	$('#lightbox-toggle4').click(function(){
+		$('#backdrop4').animate({'opacity':'.80'}, 300, 'linear').css('display', 'block');
+		$('#box4').fadeIn();
+	});
+
+	$('.close, #backdrop4').click(function(){
+		$('#backdrop4').animate({'opacity':'0'}, 300, 'linear', function(){
+			$('#backdrop4').css('display', 'none');
+		});
+		$('#box4').fadeOut();
+	});
+
+	$('.close').hover(function() {
+					$(this).css('cursor','pointer');
+			});
+
+});
+
+/*Fifth pic*/
+jQuery(document).ready(function(){
+
+	$('#lightbox-toggle5').click(function(){
+		$('#backdrop5').animate({'opacity':'.80'}, 300, 'linear').css('display', 'block');
+		$('#box5').fadeIn();
+	});
+
+	$('.close, #backdrop5').click(function(){
+		$('#backdrop5').animate({'opacity':'0'}, 300, 'linear', function(){
+			$('#backdrop5').css('display', 'none');
+		});
+		$('#box5').fadeOut();
+	});
+
+	$('.close').hover(function() {
+					$(this).css('cursor','pointer');
+			});
+
+});
+
+/*Sixth pic*/
+jQuery(document).ready(function(){
+
+	$('#lightbox-toggle6').click(function(){
+		$('#backdrop6').animate({'opacity':'.80'}, 300, 'linear').css('display', 'block');
+		$('#box6').fadeIn();
+	});
+
+	$('.close, #backdrop6').click(function(){
+		$('#backdrop6').animate({'opacity':'0'}, 300, 'linear', function(){
+			$('#backdrop6').css('display', 'none');
+		});
+		$('#box6').fadeOut();
+	});
+
+	$('.close').hover(function() {
+					$(this).css('cursor','pointer');
+			});
+
+});
+
+/*Seventh pic*/
+jQuery(document).ready(function(){
+
+	$('#lightbox-toggle7').click(function(){
+		$('#backdrop7').animate({'opacity':'.80'}, 300, 'linear').css('display', 'block');
+		$('#box7').fadeIn();
+	});
+
+	$('.close, #backdrop7').click(function(){
+		$('#backdrop7').animate({'opacity':'0'}, 300, 'linear', function(){
+			$('#backdrop7').css('display', 'none');
+		});
+		$('#box7').fadeOut();
+	});
+
+	$('.close').hover(function() {
+					$(this).css('cursor','pointer');
+			});
+
+});
+
+/*Eight pic*/
+jQuery(document).ready(function(){
+
+	$('#lightbox-toggle8').click(function(){
+		$('#backdrop8').animate({'opacity':'.80'}, 300, 'linear').css('display', 'block');
+		$('#box8').fadeIn();
+	});
+
+	$('.close, #backdrop8').click(function(){
+		$('#backdrop8').animate({'opacity':'0'}, 300, 'linear', function(){
+			$('#backdrop8').css('display', 'none');
+		});
+		$('#box8').fadeOut();
+	});
+
+	$('.close').hover(function() {
+					$(this).css('cursor','pointer');
+			});
+
+});
+
+
+/*Ninth pic*/
+jQuery(document).ready(function(){
+
+	$('#lightbox-toggle9').click(function(){
+		$('#backdrop9').animate({'opacity':'.80'}, 300, 'linear').css('display', 'block');
+		$('#box9').fadeIn();
+	});
+
+	$('.close, #backdrop9').click(function(){
+		$('#backdrop9').animate({'opacity':'0'}, 300, 'linear', function(){
+			$('#backdrop9').css('display', 'none');
+		});
+		$('#box9').fadeOut();
+	});
+
+	$('.close').hover(function() {
+					$(this).css('cursor','pointer');
+			});
+
+});
+
+/*Tenth pic*/
+jQuery(document).ready(function(){
+
+	$('#lightbox-toggle10').click(function(){
+		$('#backdrop10').animate({'opacity':'.80'}, 300, 'linear').css('display', 'block');
+		$('#box10').fadeIn();
+	});
+
+	$('.close, #backdrop10').click(function(){
+		$('#backdrop10').animate({'opacity':'0'}, 300, 'linear', function(){
+			$('#backdrop10').css('display', 'none');
+		});
+		$('#box10').fadeOut();
+	});
+
+	$('.close').hover(function() {
+					$(this).css('cursor','pointer');
+			});
+
+});
+
+
+/*Eleventh pic*/
+jQuery(document).ready(function(){
+
+	$('#lightbox-toggle11').click(function(){
+		$('#backdrop11').animate({'opacity':'.80'}, 300, 'linear').css('display', 'block');
+		$('#box11').fadeIn();
+	});
+
+	$('.close, #backdrop11').click(function(){
+		$('#backdrop11').animate({'opacity':'0'}, 300, 'linear', function(){
+			$('#backdrop11').css('display', 'none');
+		});
+		$('#box11').fadeOut();
+	});
+
+	$('.close').hover(function() {
+					$(this).css('cursor','pointer');
+			});
+
+});
+
+
+/*Twelfth pic*/
+jQuery(document).ready(function(){
+
+	$('#lightbox-toggle12').click(function(){
+		$('#backdrop12').animate({'opacity':'.80'}, 300, 'linear').css('display', 'block');
+		$('#box12').fadeIn();
+	});
+
+	$('.close, #backdrop12').click(function(){
+		$('#backdrop12').animate({'opacity':'0'}, 300, 'linear', function(){
+			$('#backdrop12').css('display', 'none');
+		});
+		$('#box12').fadeOut();
+	});
+
+	$('.close').hover(function() {
+					$(this).css('cursor','pointer');
+			});
 
 });
